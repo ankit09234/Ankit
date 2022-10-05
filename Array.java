f863bff7a96ba0b5a59a12743d2b1d6d9aaa1ca3
RAJNISH KUMAR <99047195+rajnishkumar14102000@users.noreply.github.com>
2022-10-06 01:27:47 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -3,8 +3,10 @@ class Array
 {
 public static void main(String args[])throws IOException
 {
+  
 InputStreamReader re));
 for(i=0; i<8; i++)
 System.out.println(a[i]);
+  System.out.println("HEL0");
 }
 } 
