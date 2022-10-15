2f8724483d8c6e8287ca8ec637ca9d796f0e3bf4
Nilesh Singh <91590022+s1nghn1lesh@users.noreply.github.com>
2022-10-15 14:46:29 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -4,7 +4,7 @@ class Account
 public static void main()
 {
 Scanner in=new Scanner(System.in);
-System.out.println("Press 1 for Term Deposit : ");
+System.out.println("Press 1 for Term Deposit is : ");
 System.out.println("Press 2 for Recurring Deposit : ");
 System.out.println("Enter your choice");
 int n=in.nextInt();
